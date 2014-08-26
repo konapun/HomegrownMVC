6c5f0ce45c115982083699061945b35d98caff72
torvalds <bremen.braun@gmail.com>
2014-08-26 14:14:16 -0500

    Added CSV data importer for use with FixtureModel

--- HomegrownMVC.php
+++ HomegrownMVC.php
@@ -9,6 +9,7 @@
 $homegrownBase = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'HomegrownMVC' . DIRECTORY_SEPARATOR;
 $controllerBase = $homegrownBase . 'Controller' . DIRECTORY_SEPARATOR;
 $modelBase = $homegrownBase . 'Model' . DIRECTORY_SEPARATOR;
+$importerBase = $modelBase . DIRECTORY_SEPARATOR . 'DataImporter' . DIRECTORY_SEPARATOR;
 $behaviorsBase = $homegrownBase . 'Behaviors' . DIRECTORY_SEPARATOR;
 $errorBase = $homegrownBase . 'Error' . DIRECTORY_SEPARATOR;
 
@@ -21,8 +22,11 @@ include_once($controllerBase . 'WildcardController.php');
 include_once($modelBase . 'PluralModel.php');
 include_once($modelBase . 'FixtureModel.php');
 include_once($modelBase . 'SingularModel.php');
+include_once($importerBase . 'IDataImporter.php');
+include_once($importerBase . 'CSVDataImporter.php');
 include_once($errorBase . 'BuildException.php');
 include_once($errorBase . 'MalformedUrlException.php');
 include_once($errorBase . 'ResultNotFoundException.php');
 include_once($errorBase . 'RouteNotDefinedException.php');
+include_once($errorBase . 'CSVFormatException.php');
 ?>
